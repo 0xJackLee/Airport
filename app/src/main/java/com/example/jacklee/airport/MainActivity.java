18078986dbd2f02f709b370b844392d632bf4d78
Jack <280147597@qq.com>
2019-10-08 22:56:25 +0800

    add some airport information

--- app/src/main/java/com/example/jacklee/airport/MainActivity.java
+++ app/src/main/java/com/example/jacklee/airport/MainActivity.java
@@ -14,11 +14,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<Airport> cities = new ArrayList<Airport>();
-        cities.add(new Airport("Tianjin Binhai International Airport"));
-        cities.add(new Airport("Beijing Capital International Airport"));
+        ArrayList<Airport> airports = new ArrayList<Airport>();
+        airports.add(new Airport("Tianjin Binhai International Airport"));
+        airports.add(new Airport("Beijing Capital International Airport"));
+        airports.add(new Airport("Qindao Liuting International Airport"));
+        airports.add(new Airport("Changsha Huanghua International Airport"));
+        airports.add(new Airport("Guangzhou Baiyun Internation Airport"));
+        airports.add(new Airport("Hangzhou xiaoshan International Airport"));
+        airports.add(new Airport("Shanghai Hongqiao International Airport"));
+        airports.add(new Airport("Shanghai Pudong International Airport"));
+        airports.add(new Airport("Beijing Daxing International Airport"));
+        airports.add(new Airport("Kunming Changshui International Airport"));
+        airports.add(new Airport("Chengdu Shuangliu International Airport"));
+        airports.add(new Airport("Wuhan Tianhe Internationla Airport"));
+        airports.add(new Airport("Chongqin Jiangbei International Airport"));
+        airports.add(new Airport("Shenzhen Bao'an International Airport"));
+        airports.add(new Airport("Xi'an Xianyan International Airport"));
 
-        AirportAdapter airportAdapter = new AirportAdapter(this, cities);
+        AirportAdapter airportAdapter = new AirportAdapter(this, airports);
         ListView listView = (ListView)findViewById(R.id.main_list_view);
         listView.setAdapter(airportAdapter);
     }
